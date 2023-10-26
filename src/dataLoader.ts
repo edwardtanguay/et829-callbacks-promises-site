4621fe12c23787dd01ca8689e6d08d9a29d1bbd2
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-26 10:56:02 +0200

    before callback

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,5 +1,15 @@
 import employees from './data/employees.json';
+import axios from 'axios';
 
 export const getSynchronousEmployees = () => {
 	return employees;
+}
+
+export const getAsynchronousEmployees = () => {
+	const url = 'https://edwardtanguay.vercel.app/share/employees.json';
+	(async () => {
+		const employees = (await axios.get(url)).data;
+		console.log('dataLoader', employees);
+		return employees;
+	})();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import './style.css'
 import * as dataLoader from './dataLoader';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-  <h2>Asynchronous Functions with Callbacks and Promises</h2>
+const employees = await dataLoader.getAsynchronousEmployees();
+console.log('main', employees);
 
-  <h3>Synchronous Employees</h3>
-  ${dataLoader.getSynchronousEmployees().map(m => m.firstName).join(', ')}
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+	<h2>Asynchronous Functions with Callbacks and Promises</h2>
+
+	<h3>Synchronous Employees</h3>
+	${dataLoader.getSynchronousEmployees().map(m => m.firstName).join(', ')}
+
+	<h3>Asynchronous Employees</h3>
 `
